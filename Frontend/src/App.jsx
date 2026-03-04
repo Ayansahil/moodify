5d9af48debd51f2693034389936b78cc3e00400d
Ayan Sahil <ayans8557@gmail.com>
2026-03-04 16:34:40 +0530

    add authentication and expression detection features
    - Updated package.json to include new dependencies for authentication and expression detection.
    - Created a new routing structure with app.routes.jsx for navigation.
    - Implemented authentication context and hooks for managing user state.
    - Developed login and registration pages with form handling.
    - Added face expression detection component using MediaPipe.
    - Created utility functions for initializing and detecting expressions.
    - Styled login and registration pages with SCSS.
    - Removed unused assets and organized styles for better maintainability.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes"
+import "./features/shared/styles/global.scss"
+import { AuthProvider } from "./features/auth/auth.context"
+// import { SongContextProvider } from "./features/home/song.context"
 
-const App = () => {
+function App() {
+
   return (
-    <div>App</div>
+    <AuthProvider>
+      {/* <SongContextProvider> */}
+        <RouterProvider router={router} />
+      {/* </SongContextProvider> */}
+    </AuthProvider>
   )
 }
 
