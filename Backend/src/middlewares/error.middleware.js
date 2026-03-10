528b155fd09d695ee443dcfae5df8141d947ee00
Ayan Sahil <ayans8557@gmail.com>
2026-03-10 16:46:57 +0530

    created error.middleware.js for centralized error handling

--- Backend/src/middlewares/error.middleware.js
+++ Backend/src/middlewares/error.middleware.js
@@ -4,7 +4,6 @@ const errorHandler = (err, req, res, next) => {
     res.status(statusCode);
     res.json({
         message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
         stack: process.env.NODE_ENV === 'development' ? err.stack : null,
     });
 };
